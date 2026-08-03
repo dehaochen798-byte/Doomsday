990b052e5c93ee426c528cde78fe4c3ca866a4d9
陈德浩 <1737185581@qq.com>
2026-08-03 16:47:37 +0800

    app全局导入navie-ui

--- Clint/src/main.ts
+++ Clint/src/main.ts
@@ -1,8 +1,9 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
 import router from './routers/index.ts'
+import naive from 'naive-ui'
 
 const app = createApp(App)
+app.use(naive)
 app.use(router)
 app.mount('#app')
--- Clint/src/views/Home.vue
+++ Clint/src/views/Home.vue
@@ -1,3 +1,15 @@
 <template>
-  
+    <n-layout >
+
+    </n-layout>
 </template>
+
+<style scoped>
+
+</style>
+/**
+ * ###使用n-layout布局样式失效
+ *删除项目自带多余style-css样式文件
+ *
+ *
+ */
