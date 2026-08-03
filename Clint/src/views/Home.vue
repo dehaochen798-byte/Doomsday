341a133d9aabe996ad7b2c0f2a7ab064bdac547d
陈德浩 <1737185581@qq.com>
2026-08-03 17:22:21 +0800

    #

--- Clint/src/views/Home.vue
+++ Clint/src/views/Home.vue
@@ -1,6 +1,16 @@
 <template>
-    <n-layout >
-
+    <n-layout has-sider style="height: 100vh;">
+        <n-layout-sider bordered content-style="padding: 16px;">
+            侧边栏
+        </n-layout-sider>
+        <n-layout>
+            <n-layout-header bordered content-style="padding: 16px;">
+                标题
+            </n-layout-header>
+            <n-layout-content content-style="padding: 16px;">
+                内容
+            </n-layout-content>
+        </n-layout>
     </n-layout>
 </template>
 
@@ -8,8 +18,12 @@
 
 </style>
 /**
- * ###使用n-layout布局样式失效
- *删除项目自带多余style-css样式文件
+ * ### 使用n-layout布局样式失效
+ * 删除项目自带多余style-css样式文件
  *
- *
+ * ### content-style不提示补全
+ * tsconfig.app.json的type里引入"naive-ui/volar"
+ * 
+ * ### 多个router-view同一页面使用
+ * 命名视图
  */
