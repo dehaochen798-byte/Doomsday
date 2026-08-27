29f7668dee044bc5301054d122b516edbb3504aa
陈德浩 <1737185581@qq.com>
2026-08-27 17:53:17 +0800

    #

--- Clint/src/views/text/text.vue
+++ Clint/src/views/text/text.vue
@@ -2,7 +2,10 @@
 import { onMounted } from 'vue'
 
 onMounted(() => {//❗坑 onMounted期望接受一个回调函数，dom挂载完成后调用
-  console.log('日期', Date())
+  console.log('日期:', Date(),'类型：',typeof(Date()))//日期: Thu Aug 27 2026 17:32:24 GMT+0800 (中国标准时间) 类型： string
+  console.log('日期:',new Date(),'类型：',typeof(new Date()))//日期: Thu Aug 27 2026 17:32:24 GMT+0800 (中国标准时间) 类型： object
+
+  
 })
 </script>
 
